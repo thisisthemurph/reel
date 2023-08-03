bbd7ce4e4f4877765fe7fd8a69692302c40cdd71
Michael Murphy <michael.w.murphy@outlook.com>
2023-08-03 08:20:45 +0100

    Updates logger to take log level from environment variables

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -6,12 +6,27 @@ from .filters import MinLevelAndAboveFilter
 from .supabase_handler import SupabaseHandler
 
 
+def __get_log_level_from_env() -> int:
+    """Returns the appropriate logging level. If in DEVELOPMENT uses DEBUG.
+    If LOG_LEVEL int is set in environment variables, uses that. Otherwise, WARNING."""
+    if os.getenv("ENVIRONMENT") == "development":
+        return logging.DEBUG
+
+    try:
+        return int(os.getenv("LOG_LEVEL") or "")
+    except ValueError:
+        return logging.WARNING
+
+
 def make_logger(name: str) -> logging.Logger:
+    log_level = __get_log_level_from_env()
+    print(log_level)
     supabase = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
+
     handler = SupabaseHandler(supabase)
     handler.addFilter(MinLevelAndAboveFilter(logging.WARNING))
 
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(log_level)
     logger.addHandler(handler)
     return logger
